27e200a194c21532d76fa5c08b1ca20e6bb0a67d
RevelsXIX <joshua.revels@gmail.com>
2021-05-10 13:53:49 -0400

    Refactored and made check_pwd return False if the password in question does not contain at least one digit.

--- check_pwd.py
+++ check_pwd.py
@@ -2,6 +2,8 @@ def check_pwd(pwd):
     if pwd:
         if len(pwd) < 8 or len(pwd) > 20:
             return False
-        else:
-            return True
-    return False
+        if not any(p.isdigit() for p in pwd):
+            return False
+        return True
+    else:
+        return False
